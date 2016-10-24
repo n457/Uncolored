13c172c322cde5b399464204651ed8351853e003
n457 <n457.contact@gmail.com>
2016-10-24 14:20:25 +0200

    updated website for release v.0.9.1

--- js/index.js
+++ js/index.js
@@ -1,14 +1,31 @@
 $(function () {
 
-  $DownloadButton = $('.multi-download-button');
-  $AppVersion = $DownloadButton.find('.version');
+  var $AppLargeImg = $('.app-large-img');
+  var strAppLargeImgPathBase = 'img/uncolored-large-screenshot-.png';
+  var $DownloadButton = $('.multi-download-button');
+  var $AppVersion = $DownloadButton.find('.version');
 
+  if (platform.os.family === 'Windows') {
+    $AppLargeImg.attr('src', strAppLargeImgPathBase.replace('.png', 'win.png'));
+    $AppVersion.after($DownloadButton.find('.win-setup').clone());
+  }
+  else if (platform.os.family === 'OS X') {
+    $AppLargeImg.attr('src', strAppLargeImgPathBase.replace('.png', 'osx.png'));
+    $AppVersion.after($DownloadButton.find('.osx-dmg').clone());
+  }
+  // OS family detection on Debian returns 'Linux'
+  else if (platform.os.family === 'Ubuntu') {
+    $AppLargeImg.attr('src', strAppLargeImgPathBase.replace('.png', 'linux.png'));
+    $AppVersion.after($DownloadButton.find('.deb').clone());
+  }
+  else {
+    $AppLargeImg.attr('src', strAppLargeImgPathBase.replace('.png', 'linux.png'));
+    $DownloadButton.removeClass('os-known');
+    $DownloadButton.addClass('os-unknown');
+    $DownloadButton.addClass('opened');
+  }
 
-  emojify.setConfig({ img_dir: 'lib/emojis' });
-
-  $('.contains-emojis').each(function () {
-    emojify.run(this);
-  });
+  $AppLargeImg.addClass('visible');
 
 
   $DownloadButton.find('.more-button').on('click', function () {
@@ -19,20 +36,9 @@ $(function () {
   });
 
 
-  if (platform.os.family === 'Windows') {
-    $AppVersion.after($DownloadButton.find('.win-setup').clone());
-  }
-  else if (platform.os.family === 'OS X') {
-    $AppVersion.after($DownloadButton.find('.osx-dmg').clone());
-  }
-  // OS family detection on Debian returns 'Linux'
-  else if (platform.os.family === 'Ubuntu') {
-    $AppVersion.after($DownloadButton.find('.deb').clone());
-  }
-  else {
-    $DownloadButton.removeClass('os-known');
-    $DownloadButton.addClass('os-unknown');
-    $DownloadButton.addClass('opened');
-  }
+  emojify.setConfig({ img_dir: 'lib/emojis' });
 
+  $('.contains-emojis').each(function () {
+    emojify.run(this);
+  });
 });
