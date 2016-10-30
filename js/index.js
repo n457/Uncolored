0599892ec71b0b086a272bd4711dc973a87a154b
n457 <n457.contact@gmail.com>
2016-10-30 16:27:55 +0100

    updated site for release v.0.10.1

--- js/index.js
+++ js/index.js
@@ -4,22 +4,28 @@ $(function () {
   var strAppLargeImgPathBase = 'img/uncolored-large-screenshot-.png';
   var $DownloadButton = $('.multi-download-button');
   var $AppVersion = $DownloadButton.find('.version');
+  var $NotesImg = $('.text-notes img.full-width');
+  var strNotesImgPathBase = 'img/ready-for-notes-.png';
 
   if (platform.os.family === 'Windows') {
     $AppLargeImg.attr('src', strAppLargeImgPathBase.replace('.png', 'win.png'));
+    $NotesImg.attr('src', strNotesImgPathBase.replace('.png', 'win.png'));
     $AppVersion.after($DownloadButton.find('.win-setup').clone());
   }
   else if (platform.os.family === 'OS X') {
     $AppLargeImg.attr('src', strAppLargeImgPathBase.replace('.png', 'osx.png'));
+    $NotesImg.attr('src', strNotesImgPathBase.replace('.png', 'osx.png'));
     $AppVersion.after($DownloadButton.find('.osx-dmg').clone());
   }
   // OS family detection on Debian returns 'Linux'
   else if (platform.os.family === 'Ubuntu') {
     $AppLargeImg.attr('src', strAppLargeImgPathBase.replace('.png', 'linux.png'));
+    $NotesImg.attr('src', strNotesImgPathBase.replace('.png', 'linux.png'));
     $AppVersion.after($DownloadButton.find('.deb').clone());
   }
   else {
     $AppLargeImg.attr('src', strAppLargeImgPathBase.replace('.png', 'linux.png'));
+    $NotesImg.attr('src', strNotesImgPathBase.replace('.png', 'linux.png'));
     $DownloadButton.removeClass('os-known');
     $DownloadButton.addClass('os-unknown');
     $DownloadButton.addClass('opened');
